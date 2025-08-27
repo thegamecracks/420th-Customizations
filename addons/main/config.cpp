d99ca3e448b70c03363e00850f2208294914b7e7
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-27 17:43:57 -0400

    feat: add paramedic capacity overrides

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -5,9 +5,27 @@ class CfgPatches {
         url = "https://github.com/thegamecracks/420th-Customizations";
 
         requiredVersion = 2.20;
-        requiredAddons[] = {};
+        requiredAddons[] = {
+            "A3_Characters_F_Orange",
+            "A3_Supplies_F_AoW_Bags",
+        };
         skipWhenMissingDependencies = 0;
 
         units[] = {};
+    };
+};
+
+class CfgVehicles {
+    // B_CivilianBackpack_01_Sport_Red_F
+    class B_CivilianBackpack_01_Base_F {
+        maximumLoad = 300; // 240
+    };
+};
+
+class CfgWeapons {
+    class U_C_Paramedic_01_F {
+        class ItemInfo {
+            containerClass = "Supply500"; // "Supply30"
+        };
     };
 };
