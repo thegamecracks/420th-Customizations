3834003ed36b384cccfd6f7d256aac972fa45177
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-27 17:11:37 -0400

    feat: add main addon

---
+++ addons/main/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+    class fdelta_main {
+        name = "420th Customizations";
+        author = "thegamecracks";
+        url = "https://github.com/thegamecracks/420th-Customizations";
+
+        requiredVersion = 2.20;
+        requiredAddons[] = {};
+        skipWhenMissingDependencies = 0;
+
+        units[] = {};
+    };
+};
