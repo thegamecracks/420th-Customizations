c5352e747469007dac972a96e11159bb2ddcecbb
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-01-22 14:31:04 -0500

    feat: add GM weapons compatibility by Online1034

---
+++ addons/magwells_gm/config.cpp
@@ -0,0 +1,69 @@
+class CfgPatches {
+    class fdelta_magwells_gm {
+        name = "420th Customizations - Global Mobilization Weapons Compatibility";
+        author = "ol1034, thegamecracks";
+        url = "https://github.com/thegamecracks/420th-Customizations";
+
+        requiredVersion = 2.20;
+        requiredAddons[] = {
+            // FIXME: insert GM required addons
+        };
+        skipWhenMissingDependencies = 1;
+        skipWhenAnyAddonPresent[] = {"vmagcompatibility"};
+
+        units[] = {};
+    };
+};
+
+#define MAGAZINES_556x45 \
+    "30Rnd_556x45_Stanag", \
+    "30Rnd_556x45_Stanag_green", \
+    "30Rnd_556x45_Stanag_red", \
+    "30Rnd_556x45_Stanag_Tracer_Red", \
+    "30Rnd_556x45_Stanag_Tracer_Green", \
+    "30Rnd_556x45_Stanag_Tracer_Yellow", \
+    "30Rnd_556x45_Stanag_Sand", \
+    "30Rnd_556x45_Stanag_Sand_green", \
+    "30Rnd_556x45_Stanag_Sand_red", \
+    "30Rnd_556x45_Stanag_Sand_Tracer_Red", \
+    "30Rnd_556x45_Stanag_Sand_Tracer_Green", \
+    "30Rnd_556x45_Stanag_Sand_Tracer_Yellow", \
+    "150Rnd_556x45_Drum_Green_Mag_F", \
+    "150Rnd_556x45_Drum_Green_Mag_Tracer_F", \
+    "150Rnd_556x45_Drum_Sand_Mag_F", \
+    "150Rnd_556x45_Drum_Sand_Mag_Tracer_F", \
+    "150Rnd_556x45_Drum_Mag_F", \
+    "150Rnd_556x45_Drum_Mag_Tracer_F",
+
+class CfgMagazineWells {
+    class gm_magazineWell_473x33mm_g11 {
+        BI_mags[] = {
+            "30Rnd_65x39_caseless_green_mag_Tracer",
+            "30Rnd_65x39_caseless_green",
+        };
+    };
+    class gm_magazineWell_556x45mm_hk33 {
+        BI_mags[] = {
+            MAGAZINES_556x45
+        };
+    };
+    class gm_magazineWell_556x45mm_sg550 {
+        BI_mags[] = {
+            MAGAZINES_556x45
+        };
+    };
+    class gm_magazineWell_762x51mm_mg8 {
+        BI_links[] = {
+            "150Rnd_762x51_Box",
+            "150Rnd_762x51_Box_Tracer",
+            "150Rnd_762x54_Box",
+            "150Rnd_762x54_Box_Tracer",
+        };
+    };
+    class gm_magazineWell_762x51mm_sg542 {
+        BI_mags[] = {
+            "10Rnd_Mk14_762x51_Mag",
+            "20Rnd_762x51_Mag",
+        };
+    };
+};
