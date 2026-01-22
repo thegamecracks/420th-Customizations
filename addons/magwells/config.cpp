4b2f5fbb0419f62436fa720844a16fafb9cb9fae
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-01-22 14:38:54 -0500

    fix: remove 3GLs from CBA M203 by Online1034

--- addons/magwells/config.cpp
+++ addons/magwells/config.cpp
@@ -333,23 +333,6 @@ class CfgMagazineWells {
             "5Rnd_127x108_APDS_Mag",
         };
     };
-    class CBA_40mm_M203 {
-        BI_grenades[] = {
-            "3Rnd_HE_Grenade_shell",
-            "3Rnd_Smoke_Grenade_shell",
-            "3Rnd_SmokeRed_Grenade_shell",
-            "3Rnd_SmokeGreen_Grenade_shell",
-            "3Rnd_SmokeYellow_Grenade_shell",
-            "3Rnd_SmokePurple_Grenade_shell",
-            "3Rnd_SmokeBlue_Grenade_shell",
-            "3Rnd_SmokeOrange_Grenade_shell",
-            "3Rnd_UGL_FlareWhite_F",
-            "3Rnd_UGL_FlareGreen_F",
-            "3Rnd_UGL_FlareRed_F",
-            "3Rnd_UGL_FlareYellow_F",
-            "3Rnd_UGL_FlareCIR_F",
-        };
-    };
     class CBA_40mm_GP {
         BI_gredammo[] = {
             MAGAZINES_GL
