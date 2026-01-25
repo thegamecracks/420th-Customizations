fe53493df799d330fc82e1476df8613d3804933e
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-01-25 07:33:05 -0500

    docs: add ol1034 author to main addon

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,7 +1,7 @@
 class CfgPatches {
     class fdelta_main {
         name = "420th Customizations";
-        author = "thegamecracks";
+        author = "ol1034, thegamecracks";
         url = "https://github.com/thegamecracks/420th-Customizations";
 
         requiredVersion = 2.20;
